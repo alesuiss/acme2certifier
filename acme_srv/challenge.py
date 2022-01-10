2aa9f73abf631362869c927c1f16e82ff4194f60
Arthur Lesuisse <arthur.lesuisse@ulb.be>
2022-01-10 10:40:42 +0100

    disabled useless challenge types

--- acme_srv/challenge.py
+++ acme_srv/challenge.py
@@ -424,7 +424,7 @@ class Challenge(object):
         self.logger.debug('Challenge.new_set({0}, {1})'.format(authz_name, token))
         challenge_list = []
         if not tnauth:
-            for challenge_type in ['http-01', 'dns-01', 'tls-alpn-01']:
+            for challenge_type in ['http-01']:
                 challenge_json = self._new(authz_name, challenge_type, token)
                 if challenge_json:
                     challenge_list.append(challenge_json)
